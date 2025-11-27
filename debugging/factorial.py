9891d1ad9e8a4efc6bca2ab11373748ec529b39b
root <root@ip-10-42-251-6.ec2.internal>
2025-11-27 23:02:20 +0000

    Fix factorial infinite loop bug

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,11 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1©
+        n -= 1
     return result
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        f = factorial(int(sys.argv[1]))
-        print(f)
-    else:
-        print("Usage: ./factorial.py <number>")
+
+f = factorial(int(sys.argv[1]))
+print(f)
